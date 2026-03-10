faee4adb10b5931a2e8328e481cf0b7b688aa6a9
Shalini Gupta <chinkiwork28@gmail.com>
2026-03-10 22:03:31 +0530

    Add files via upload

---
+++ src/jpa_mapping/src/main/java/org/example/entity/Student.java
@@ -0,0 +1,22 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String course;
+
+    @OneToOne
+    @JoinColumn(name = "laptop_id", unique = true)
+    private Laptop laptop;
+}
